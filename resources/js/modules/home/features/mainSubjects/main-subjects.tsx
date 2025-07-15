9dce4b2c04b4a5da0b52df2f1876ba946617b8ec
Amrkhaled66 <amrkahlid77@gmail.com>
2025-07-15 19:11:13 +0300

    fix(homePage): ensure mobile slider stretches to full width

--- resources/js/modules/home/features/mainSubjects/main-subjects.tsx
+++ resources/js/modules/home/features/mainSubjects/main-subjects.tsx
@@ -7,7 +7,6 @@ import subjects from './subjects';
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-
 const MainSubjects = () => {
     const chunkedSubjects = [];
     for (let i = 0; i < subjects.length; i += 4) {
@@ -16,7 +15,7 @@ const MainSubjects = () => {
 
     return (
         <div className="bg-[#F4F4F4]">
-            <div className="container space-y-[60px] py-sectionPadding lg:space-y-[80px]">
+            <div className="space-y-[60px] px-2  py-sectionPadding lg:container lg:space-y-[80px]">
                 <SectionTitle icon="bxs:book" title="المواد الدراسية" subTitle="اكتشف اهم المواد الدراسية" />
 
                 <div className="hidden grid-cols-4 gap-6 lg:grid">
@@ -25,7 +24,25 @@ const MainSubjects = () => {
                     ))}
                 </div>
                 <div className="block lg:hidden">
-                    <Swiper spaceBetween={16} slidesPerView={1.1}>
+                    <Swiper
+                        className=" !pr-3 sm:!pr-9"
+                        spaceBetween={16}
+                        slidesPerView={1.1}
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 1.1,
+                                spaceBetween: 16,
+                            },
+                            744: {
+                                slidesPerView: 2.2,
+                                spaceBetween: 16,
+                            },
+                            1024: {
+                                slidesPerView: 3.1,
+                                spaceBetween: 16,
+                            },
+                        }}
+                    >
                         {chunkedSubjects.map((group, index) => (
                             <SwiperSlide key={index}>
                                 <div className="flex flex-col gap-4">
