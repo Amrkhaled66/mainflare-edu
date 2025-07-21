3e3a175ab2ffc51657d166bbecb604f2f49e8f6c
Amrkhaled66 <amrkahlid77@gmail.com>
2025-07-21 03:03:34 +0300

    feat: add scrollToTop functionality

--- resources/js/layout.tsx
+++ resources/js/layout.tsx
@@ -4,10 +4,12 @@ import PageNav from './shared/components/PageNav';
 
 import { Outlet, useLocation } from 'react-router';
 import LoadingBar from './shared/components/LoadingBar';
+import ScrollToTop from './shared/components/ScrollToTop';
 const layout = () => {
     const { pathname } = useLocation();
     return (
         <div dir="rtl">
+            <ScrollToTop />
             <Header />
             <LoadingBar />
             <main className={`pt-[100px]`}>
